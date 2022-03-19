2ae72ffdd2d53afa9c3fa1e98aad34398858646f
Minju-Jeon <minju033177@gmail.com>
2022-03-19 15:19:38 +0900

    배경이미지 랜덤 적용 / 폴더삽입

---
+++ src/js/randomBackground.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+const img = [
+   "0.jpg",
+   "1.jpg",
+   "2.jpg",
+   "3.jpg",
+   "4.jpg",
+   "5.jpg",
+   "6.jpg",
+   "7.jpg",
+   "8.jpg",
+   "9.jpg"
+];
+const rdmImg = img[Math.floor(Math.random()*img.length)];
+
+function changeBgImgRnd(){
+    body.style.backgroundImage = `url("src/img/${rdmImg}")`;
+}
+
+changeBgImgRnd();
