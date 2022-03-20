365fe53a13edb48feecabe351d7ee4c9bcf95ead
jeon minju <minju033177@gmail.com>
2022-03-20 18:48:46 +0900

    시계 수정

--- NomadCoders/VanillaJS/src/js/clock.js
+++ NomadCoders/VanillaJS/src/js/clock.js
@@ -2,21 +2,19 @@ const date = document.querySelector("#clock .date");
 const time = document.querySelector("#clock .time .time");
 const switch24btn = document.querySelector("#clock #swith24hbtn");
 const dayOrNight = document.querySelector("#clock .dayOrNight");
-const T = "true";
-const F = "false";
+const T = 24;
+const F = 12;
 
 //시간표기법 변경
 function switch24() {
-    if (localStorage.getItem("24h") === T) {
+    if (localStorage.getItem("24h") == T) {
         //24시간 표기중 버튼 클릭할경우 12시간 표기로
         localStorage.setItem("24h", F);
-        switch24btn.innerHTML = "to 24h";
         console.log("24h: false");
     } else {
         //12시간 표기중 버튼 클릭할경우 24시간 표기로
         localStorage.setItem("24h", T);
         console.log("24h: true");
-        switch24btn.innerHTML = "to 12h";
     }
     dayOrNight.classList.toggle("hidden");
     getClock();
@@ -35,7 +33,7 @@ function getClock() {
 
     date.innerHTML = `${year}/${month}/${day} ${weekDay}`;
     //설정값에 따라 12~24시간 표기법으로 변경
-    if (localStorage.getItem("24h") === T) {
+    if (localStorage.getItem("24h") == T) {
         print24Clock(hour, min, sec);
     } else {
         print12Clock(hour, min, sec);
@@ -44,17 +42,12 @@ function getClock() {
 
 //24시간표기
 function print24Clock(hour, min, sec) {
-    // if (!dayOrNight.classList.contains("hidden")) {
-    //     dayOrNight.classList.toggle("hidden");
-    // }
+    switch24btn.innerHTML = "to 12h";
     time.innerHTML = `${hour}:${min}:${sec}`;
 }
 
 //12시간표기
 function print12Clock(hour, min, sec) {
-    // if (dayOrNight.classList.contains("hidden")) {
-    //     dayOrNight.classList.toggle("hidden");
-    // }
     const hourFor12 = hour % 12;
     if (hour > 11) {
         if (hour == 12) {
@@ -64,9 +57,13 @@ function print12Clock(hour, min, sec) {
         dayOrNight.innerHTML = "오후";
     } else {
         dayOrNight.innerHTML = "오전";
-
     }
-    time.innerHTML = `${hourFor12.toString().padStart(2,"0")}:${min}:${sec}`;
+    switch24btn.innerHTML = "to 24h";
+    time.innerHTML = `${hourFor12.toString().padStart(2, "0")}:${min}:${sec}`;
+}
+
+if (localStorage.getItem("24h") == T && !dayOrNight.classList.contains("hidden")) {
+    dayOrNight.classList.toggle("hidden");
 }
 
 getClock();
