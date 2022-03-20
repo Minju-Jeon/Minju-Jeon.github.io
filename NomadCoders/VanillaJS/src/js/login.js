6519fae09744b42c138faa403aceb98fcfab6004
jeon minju <minju033177@gmail.com>
2022-03-20 21:17:00 +0900

    수정

--- NomadCoders/VanillaJS/src/js/login.js
+++ NomadCoders/VanillaJS/src/js/login.js
@@ -3,21 +3,33 @@ const nameInput = loginForm.querySelector("input");
 const nameArea = document.getElementById("name");
 
 function getLogin(event) {
-  event.preventDefault();
-  localStorage.setItem("username", nameInput.value);
-  printname();
+    event.preventDefault();
+    localStorage.setItem("username", nameInput.value);
+    printname();
 }
 
 function printname() {
-  const name = localStorage.getItem("username");
-  document.querySelector("#name span").innerHTML = `${name}의 TODO LIST`;
-  loginForm.classList.toggle("hidden");
-  nameArea.classList.toggle("hidden");
-  console.log(`${name} 로그인`);
+    const name = localStorage.getItem("username");
+    document.querySelector("#name span").innerHTML = `${name}의 TODO LIST`;
+    loginForm.classList.toggle("hidden");
+    nameArea.classList.toggle("hidden");
+    console.log(`${name} 로그인`);
+}
+
+
+function placeholderChangerIn() {
+    nameInput.placeholder = "이름을 적고 엔터";
+}
+
+function placeholderChangerOut() {
+    const temp = nameInput.placeholder.substring[0, 1];
+    nameInput.placeholder = temp;
 }
 
 if (localStorage.getItem("username")) {
-  printname();
+    printname();
 }
 
+
+nameInput.addEventListener("focus", placeholderChangerIn);
 loginForm.addEventListener("submit", getLogin);
